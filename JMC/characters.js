0da247ff05505ef56cb0f31de2a36503ed4b3aa0
mgraham33 <grahammatt33@gmail.com>
2021-03-02 15:19:16 -0600

    made the skeleton for the character selection

--- JMC/characters.js
+++ JMC/characters.js
@@ -1,4 +1,22 @@
-let balance = 0;
-let salary = 0;
+let balance = -1;
+let salary = -1;
 let job = "";
 let name = "";
+
+//TODO
+if (this.name === "TODO"){
+    job = "TODO";
+    salary = -1;
+    balance = -1;
+} else if (this.name === "TODO"){
+    job = "TODO";
+    salary = -1;
+    balance = -1;
+} else if (this.name === "TODO"){
+    job = "TODO";
+    salary = -1;
+    balance = -1;
+}
+else {
+    console.log("PROGRAMMING ERROR!!!!");
+}
